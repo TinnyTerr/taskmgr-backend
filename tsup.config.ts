acf20d6eed691153714eccbdd45cd5fb35c61975
TinnyTerr <168141209+TinnyTerr@users.noreply.github.com>
2024-10-14 17:28:48 +0100

    compiler settings
    Signed-off-by: TinnyTerr <168141209+TinnyTerr@users.noreply.github.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
+	ignoreWatch: ["node_modules"],
 	splitting: true,
 	sourcemap: true,
 	clean: true,
+	tsconfig: "./tsconfig.json",
 });
